c8fbfdd8071913dd9fb8165fd66e77f2eb8ab484
Topphat <124810522+Topphat@users.noreply.github.com>
2023-03-04 12:00:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.title('Анализ финансовых данных компании')
 ticker = 'AAPL'
 
 def load_data(url):
-  r = requests.get(url,headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+  r = requests.get(url)
   data = pd.read_html(r.text)
   return data
 
